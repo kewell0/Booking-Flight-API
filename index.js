306d9174d6815d66c3e1075f2439434902ae93c2
CALEB <owatahcaleb@gmail.com>
2022-11-19 02:47:35 +0100

    delete

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const models = require("./models/Flight");
 const routes = require("./routes/flightRoute");
 
 const app = express();
-const flightDetails = require('./flightDetails.json');
-const fs = require('fs');
+
+
+
+let flightDetails = [
+  {
+  id: 1,
+  title: "flight to canada",
+  time: "1pm",
+  price: 26000,
+  date: "26-06-2022"
+}
+]
 
 app.use(json());
 
@@ -16,15 +26,8 @@ app.use("/", routes);
 // ****ADD/BOOK A FLIGHT**** 
 app.post('/flightDetails', (req, res) => {
   
-  flightDetails.push(req.body.newFlightDetail);
-
-  let stringData = JSON.stringify(flightDetails, null, 2);
-  fs.writeFile('flightDetails.json', stringData, (err) => {
-    if(err) {
-      return res.status(500).json({message: err})
-    }
-  })
-
+  flightDetails.push(req.body);
+  // console.log(req.body)
   return res.status(200).json({message:"New flight booked"});
 })
 
@@ -47,7 +50,38 @@ app.get('/flightDetails/:id', (req, res) => {
   }
 })
 
+// *****UPDATE/EDIT FLIGHT*****
+app.put('/flightDetails/:id', (req, res) => {
+  flightDetails.findByIdAndUpdate(req.params.id, {
+    time: req.body.time,
+    date: req.body.date
+  }, (err, flightDetail) => {
+    if(err) {
+      return res.status(500).json({message: err})
+    } else if(!flightDetail) {
+      return res.status(404).json({message: "Flight detail not found"})
+    } else {
+      flightDetail.save((err, updatedDetail) => {
+        if(err) {
+          return res.status(400).json({message: err})
+        } else {
+          return res.status(200).json({message: "Flight detail updated"})
+        }
+      }) 
+    }
+  })
+});
 
+
+// ******DELETE A FLIGHT********
+app.delete('/flightDetails/:id', (req, res) => {
+  const {id} = req.params;
+ 
+
+  flightDetails = flightDetails.filter((flightDetail) => flightDetail.id !== id);
+  
+  return res.status(200).json({message:"flight deleted"});  
+})
 
 
 const port = process.env.PORT || 3000;
